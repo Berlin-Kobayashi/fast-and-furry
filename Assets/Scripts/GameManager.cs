ee1dc5d4344424542fd21ad2ad42e5e618a513d6
Bogdan Nistor <Bogdan Nistor>
2016-06-11 16:16:06 +0200

    refactor

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+using System;
+
+public class GameManager : MonoBehaviour {
+
+    public Text timerText;
+    public Text addressText;
+    public int timer = 30;
+    public GameObject panel;
+
+	void Start () {
+        StartCoroutine(CountdownRoutine());
+	}
+	
+	void Update () {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            TriggerStart();
+        }
+       
+	}
+
+    private void TriggerStart()
+    {
+        if (Time.timeScale == 0)
+            Time.timeScale = 1;
+        else
+            Time.timeScale = 0;
+    }
+
+    IEnumerator CountdownRoutine()
+    {
+        int i = 1;
+        while (timer > i)
+        {
+            timer -= 1;
+            timerText.text = timer.ToString();
+            yield return new WaitForSeconds(1);
+        }
+        timerText.gameObject.SetActive(false);
+        addressText.gameObject.SetActive(false);
+        panel.gameObject.SetActive(false);
+      
+    }
+
+}
--- Assets/Scripts/SocketIOC.cs
+++ Assets/Scripts/SocketIOC.cs
@@ -16,19 +16,20 @@ public class SocketIOC : MonoBehaviour {
     
     void Start ()
 	{
+		Debug.Log (serverAddress);
         messageQueue = new Queue<IMessage>();
 
         socket = new Client(serverAddress);
 		socket.On("connect", (fn) => {
 			Debug.Log ("connect - socket");
-			socket.Emit("add user", "user_unity");
+			socket.Emit("add-user", "user_unity");
 		});
-		socket.On("user joined", (data) => {
- //           Debug.Log(data);
+		socket.On("user-joined", (data) => {
+            Debug.Log(data);
             messageQueue.Enqueue(data);
         });
-        socket.On("set vector2D", (data) => {
-//            Debug.Log(data);
+        socket.On("set-vector2D", (data) => {
+            Debug.Log(data);
             messageQueue.Enqueue(data);
         });
         socket.Error += (sender, e) => {
@@ -49,10 +50,10 @@ public class SocketIOC : MonoBehaviour {
 
             switch (message.Json.name)
             {
-                case "user joined":
+                case "user-joined":
                     GetComponent<GameController>().spawnPlayer( (string)args.Values.ElementAt(0));
                     break;
-                case "set vector2D":
+                case "set-vector2D":
 
                     Vector2 direction = new Vector2(Convert.ToSingle(args.Values.ElementAt(0)), Convert.ToSingle(args.Values.ElementAt(1)));
                     GetComponent<GameController>().movePlayer((string)args.Values.ElementAt(2), direction);
